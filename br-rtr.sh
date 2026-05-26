4c0968ceed9cb11ca8951d09486cfca849dd9abc
S4NPED <Lord.super.90@mail.ru>
2026-05-26 11:31:27 +0700

    Update br-rtr.sh

--- br-rtr.sh
+++ br-rtr.sh
@@ -89,30 +89,6 @@ router ospf
 line vty
 !
 EOF
-apt install dnsmasq -y
-cat > /etc/dnsmasq.conf << 'EOF'
-interface=ens3 
-server=8.8.8.8 
-domain=au-team.irpo 
-listen-address=192.168.100.2 
-no-resolv 
-no-hosts 
-address=/hq-rtr.au-team.irpo/192.168.100.1 
-ptr-record=1.100.168.192.in-addr.arpa,hq-rtr.au-team.irpo 
-address=/br-rtr.au-team.irpo/192.168.200.1 
-address=/hq-srv.au-team.irpo/192.168.100.2 
-ptr-record=2.100.168.192.in-addr.arpa,hq-srv.au-team.irpo 
-address=/hq-cli.au-team.irpo/192.168.100.34 
-ptr-record=34.100.168.192.in-addr.arpa,hq-cli.au-team.irpo 
-address=/br-srv.au-team.irpo/192.168.200.2 
-ptr-record=2.200.168.192.in-addr.arpa,br-srv.au-team.irpo 
-address=/docker.au-team.irpo/172.16.1.1 
-address=/web.au-team.irpo/172.16.2.1
-EOF
-cat > /etc/resolv.conf << 'EOF'
-search localdomain au-team.irpo
-nameserver 127.0.0.1
-EOF
 
 rm /root/.bash_history
 history -c
@@ -130,5 +106,3 @@ nano /etc/frr/daemons
 systemctl restart frr
 nano /etc/frr/frr.conf
 timedatectl set-timezone Asia/Krasnoyarsk
-apt install dnsmasq -y
-nano /etc/dnsmasq.conf
