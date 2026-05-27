4dab27bb1a7b4322c3dc34cbe2de738168da040a
S4NPED <Lord.super.90@mail.ru>
2026-05-27 17:26:35 +0700

    Update br-srv.sh

--- br-srv.sh
+++ br-srv.sh
@@ -68,11 +68,11 @@ EOF
 cat > /etc/ansible/hosts << 'EOF'
 [hq]
 192.168.100.1 ansible_user=net_admin ansible_password=P@ssw0rd
-192. 168.100.2 ansible_user=sshuser ansible_password=P@ssw0rd ansible_port=2026
-192. 168.100.35 ansible_user=sshuser ansible_password=P@ssw0rd
+192.168.100.2 ansible_user=sshuser ansible_password=P@ssw0rd ansible_port=2026
+192.168.100.35 ansible_user=sshuser ansible_password=P@ssw0rd
 
 [br]
-192. 168.200.1 ansible_user=net_admin ansible_password=P@ssw0rd
+192.168.200.1 ansible_user=net_admin ansible_password=P@ssw0rd
 
 [all: vars]
 ansible_python_interpreter=/usr/bin/python3.13
