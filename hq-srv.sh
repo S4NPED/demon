f6a4e99f18814d459193bff202a06522cdaabffa
S4NPED <Lord.super.90@mail.ru>
2026-05-28 22:43:26 +0700

    Update hq-srv.sh

--- hq-srv.sh
+++ hq-srv.sh
@@ -56,6 +56,8 @@ EOF
 apt install chrony -y
 sed -i '5c #pool 2.debian.pool.ntp.org iburst' /etc/chrony/chrony.conf
 sed -i '5a server 172.16.1.1 iburst' /etc/chrony/chrony.conf
+sed -i '2c search localdomain au-team.irpo' /etc/resolv.conf
+sed -i '3c nameserver 192.168.100.2' /etc/resolv.conf
 
 rm /root/.bash_history
 history -c
