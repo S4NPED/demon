bdf58eebde498e4d248c9848cf0b35107a887f2a
S4NPED <Lord.super.90@mail.ru>
2026-05-29 13:18:13 +0700

    Update hq-srv.sh

--- hq-srv.sh
+++ hq-srv.sh
@@ -93,12 +93,12 @@ systemctl enable --now apache2
 mount /root/Additional.iso /mnt/
 cp /mnt/web/index.php /var/www/html
 cp /mnt/web/logo.png /var/www/html
-sed -i '3c $username = "webc";' /var/www/html/index.php
+sed -i '3c $username = "web";' /var/www/html/index.php
 sed -i '4c $password = "P@ssw0rd";' /var/www/html/index.php
 sed -i '5c $dbname = "webdb";' /var/www/html/index.php
 mariadb –u root
 read -p "Нажмите Enter для продолжения..."
-mariadb –u webc –p –D webdb < /mnt/web/dump.sql
+mariadb –u web –p –D webdb < /mnt/web/dump.sql
 rm /var/www/html/index.html
 systemctl enable --now apache2 && systemctl restart apache2
 sed -i '1c <VirtualHost *:8080>";' /etc/apache2/sites-available/000-default.conf
