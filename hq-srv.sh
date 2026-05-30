9bf04b77442c2a196698d7a957d1dcaf41d7a0a0
S4NPED <Lord.super.90@mail.ru>
2026-05-30 14:19:03 +0700

    Update hq-srv.sh

--- hq-srv.sh
+++ hq-srv.sh
@@ -13,7 +13,8 @@ iface lo inet loopback
 
 auto ens3
 iface ens3 inet static
-address 192.168.100.2/27
+address 192.168.100.2
+netmask 255.255.255.224
 gateway 192.168.100.1
 EOF
 sed -i '51a sshuser ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
